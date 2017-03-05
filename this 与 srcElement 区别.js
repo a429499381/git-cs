8e3bff0d89e1fa8644227e4ad95f8b5b08db6d3c
429499381 <429499381@qq.com>
2017-03-06 01:53:47 +0800

    操作元素的 方法。增 删 该 查

--- this 与 srcElement 区别.js
+++ this 与 srcElement 区别.js
@@ -12,3 +12,29 @@ this和window.event.srcElement的使用区别是：
 
 事件代理详细解释 http://www.cnblogs.com/liugang-vip/p/5616484.html
 
+  e = e || window.event;
+var el = e.srcElement || e.target;   兼容 event事件写法。
+
+
+event.srcElement 事件源元素
+
+ui.parentNode  parentElement  ui元素的父元素  两个用法相同
+
+ui.nextElementSibling  ui父元素下 下一个元素 不含文本节点。
+ui.previousElementSibling     ui父元素下  上一个元素  不含文本节点
+
+ui.children[i]  ui父元素下第i个标签
+
+ui.removeChild(li)  ui为父元素， li为父元素下的子元素
+
+ui.setAttribute('total属性名',属性值)   设置UI元素属性  total 的属性值
+ui.getAttribute('total属性名',属性值)   获取UI元素属性  total 的属性值
+
+ui.innerHTML ="要写入的内容"   设置要写入标签的内容， 不要 = 号就是获取
+
+克隆元素  cloneNode(true)
+var node=document.getElementById("myList2").lastChild.cloneNode(true);
+document.getElementById("myList1").appendChild(node);
+
+
+ui.nodeType ui标签的属性   1 文本  3 标签
