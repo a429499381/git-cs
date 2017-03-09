2131d2e41774a0a9dc9120f80b2b538580472eb9
a429499381 <429499381@qq.com>
2017-03-09 10:58:51 +0800

    JS 调试插件的安装方法

--- 缓存的笔记.js
+++ 缓存的笔记.js
@@ -28,6 +28,7 @@ localStorage.removeItem("csdn"); //删除一个键值对  sessionStorage同上
 // indexdDB  和 web SQL
 // 浏览器支持 chrome 11+ firefox 4+ ie 10+  opera 不支持
 // http://bbs.9ria.com/thread-241664-1-1.html 案例网址
+http://www.cnblogs.com/wzh2010/archive/2012/05/23/2514809.html
 
 
 
